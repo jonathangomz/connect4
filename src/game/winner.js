04413bcb654760768fed4e6a8c4ebd1190c71dbe
jonathangomz <jonicgp97@gmail.com>
2024-04-09 10:54:12 -0600

    Adds winner on diagonal

--- src/game/winner.js
+++ src/game/winner.js
@@ -7,22 +7,27 @@ class WinnerValidator {
 
   static winnerMovement({ board, lastRow, lastCol, tokenForEmpty, consecutiveToWin }) {
     this.board = board;
-    this.lastRow = lastRow;
-    this.lastCol = lastCol;
+    this.currentRow = Number.parseInt(lastRow);
+    this.currentColumn = Number.parseInt(lastCol);
     this.tokenForEmpty = tokenForEmpty;
     this.consecutiveToWin = consecutiveToWin;
 
-    return (this.winnerOnRow() || this.winnerOnColumn());
+    return (this.winnerOnRow || this.winnerOnColumn || this.winnerOnDiagonal);
   }
 
-  static winnerOnRow() {
-    const current_row = this.board[this.lastRow];
+  static get winnerOnRow() {
+    const current_row = this.board[this.currentRow];
     return this.haveWinnerConsecutives(current_row);
   }
 
-  static winnerOnColumn() {
+  static get winnerOnColumn() {
     const current_col = this.generateArrayFromCurrentColumn();
     return this.haveWinnerConsecutives(current_col);
+  }
+
+  static get winnerOnDiagonal() {
+    const currentDiagonal = this.generateArrayFromDiagonal();
+    return this.haveWinnerConsecutives(currentDiagonal);
   }
 
   static haveWinnerConsecutives(arr) {
@@ -36,7 +41,6 @@ class WinnerValidator {
       }
       if(item === current_token) {
         consecutive_equals += 1;
-        
       }
       else {
         current_token = item;
@@ -53,9 +57,48 @@ class WinnerValidator {
   static generateArrayFromCurrentColumn() {
     let column_array = new Array();
     for (let row = 0; row < this.board.length; row++) {
-      column_array.push(this.board[row][this.lastCol]);
+      column_array.push(this.board[row][this.currentColumn]);
     }
     return column_array;
+  }
+
+  static generateArrayFromDiagonal() {
+    let diagonalArray = new Array();
+
+    let initialRow = this.currentRow;
+    let initialCol = this.currentColumn;
+
+    if(this.currentRow === 0 || this.currentColumn === this.board[this.currentRow].length) {
+      // If is first row or last column then just invert the values
+      initialRow = this.currentColumn;
+      initialCol = this.currentRow;
+    } else if (this.currentColumn === 0 || this.currentRow === (this.board.length - 1)) {
+      // If is last row or first column then already are the initial values
+      initialRow = this.currentRow;
+      initialCol = this.currentColumn;
+    } else {
+      // Distance between the first column to the current column
+      let distanceColumn = this.currentColumn;
+
+      // Distance between the current row to the last row in the board
+      let distanceRow = (this.board.length - 1) - this.currentRow;
+
+      // Get the shortest distance
+      // If is equal then it doesn't matter
+      let shortestDistance = distanceRow >= distanceColumn ? distanceColumn : distanceRow;
+
+      // Use the shortest distance to calculate the initial row and column for the diagonal
+      initialRow = this.currentRow + shortestDistance;
+      initialCol = this.currentColumn - shortestDistance;
+    }
+
+    console.log(initialRow, initialCol);
+    // Get the array from the diagonal
+    for (let row = initialRow, col = initialCol; (row >= 0 && col < this.board[this.currentRow].length); row--, col++) {
+      diagonalArray.push(this.board[row][col]);
+    }
+
+    return diagonalArray;
   }
 }
 
