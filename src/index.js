cabf0fe178960babc83d3ad70c6dc335aa6fe896
jonathangomz <jonicgp97@gmail.com>
2024-04-09 12:04:01 -0600

    Increments package version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 #! /usr/bin/env node
-
 const program = require('commander');
 const Match = require('./game');
 
-program.version('0.1.0');
+program.version(require('../package.json').version);
 program
   .command('start')
   .description('Start a Connect4 Game')
